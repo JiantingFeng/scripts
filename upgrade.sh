79296ae7f5db8fc1ef0d4606d915a38fbfec2f73
JiantingFeng <gianting01@gmail.com>
2022-12-14 16:40:05 +0800

    feat: remove useless commands

--- upgrade.sh
+++ upgrade.sh
@@ -13,33 +13,29 @@ brew update
 brew upgrade
 brew cleanup
 
-# Upgrade pip
-echo "Upgrade pip..."
-pip3 install --upgrade pip
-pip3 install --upgrade setuptools
-pip3 install --upgrade wheel
-pip3 install --upgrade virtualenv
-# Upgrade npm
-echo "Upgrade npm..."
-npm install -g npm
-npm update -g
-
-# Upgrade yarn
-echo "Upgrade yarn..."
-yarn global upgrade
-
-# Upgrade pnpm
-echo "Upgrade pnpm..."
-pnpm install -g pnpm
-pnpm update -g
-
+# # Upgrade pip
+# echo "Upgrade pip..."
+# pip3 install --upgrade pip
+# pip3 install --upgrade setuptools
+# pip3 install --upgrade wheel
+# pip3 install --upgrade virtualenv
+# # Upgrade npm
+# echo "Upgrade npm..."
+# npm install -g npm
+# npm update -g
+#
+# # Upgrade yarn
+# echo "Upgrade yarn..."
+# yarn global upgrade
+#
+# # Upgrade pnpm
+# echo "Upgrade pnpm..."
+# pnpm install -g pnpm
+# pnpm update -g
+#
 # Backup the system
-echo "Backup the system..."
-mackup backup
-
-# Vcpkg
-echo "Upgrade vcpkg..."
-vcpkg upgrade
+# echo "Backup the system..."
+# mackup backup
 
 # Upgrade nvim packages via packer
 echo "Upgrade nvim packages..."
